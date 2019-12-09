e9605070f21dd759659060531276041cfa4a8e99
kadirumac <kadirumac49@gmail.com>
2019-12-09 11:21:51 +0100

    aufagben sind fertig

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Work on your tasks here
+
+$(document).ready(function(){
+    $("img").click(function(){
+      $(".image.teapot").css("transform", "rotate(20deg)");
+    });
+  });
+
+
+
+  $('main p').hover(function(){
+    $(this).text("Rich in flavours");
+},function(){
+    $(this).text("Great tea for a cold night");
+});
